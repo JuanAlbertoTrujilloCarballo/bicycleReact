f14279347b19aeca1941884d16092c017c64269b
AlbertoTC <albertotrujillocarballo@gmail.com>
2022-04-08 16:50:42 +0100

    add counter button 2.0

--- src/components/counter.js
+++ src/components/counter.js
@@ -3,10 +3,15 @@ import {useState} from 'react';
 function Counter() {
     const [count, setCount] = useState(0);
 
+    const addOneToCounter = () => {
+        setCount(count +1);
+    }
+
     return (
         <>
             <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
+            <button onClick={addOneToCounter}>
+                {/* () => setCount(count + 1) */}
                 Click me
             </button>
         </>
